d086469ba7d2809c768028b5a2a85f15fed48788
szabinalovas <szabinaa.lovas@gmail.com>
2022-07-05 14:31:29 +0200

    init project

---
+++ src/test/java/com/codecool/supersprinter2000javaszabinalovas/SuperSprinter2000JavaSzabinalovasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.supersprinter2000javaszabinalovas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSprinter2000JavaSzabinalovasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
